b29b5746f08fc78b6fe53a57dedbfbf20983cd50
Marijn-1999 <marijn1999@home.nl>
2025-04-08 14:29:16 +0200

    Hover highlights toegevoegd

--- app/project1/page.jsx
+++ app/project1/page.jsx
@@ -1,10 +1,17 @@
 import Image from "next/image";
+import Link from "next/link"; // import Link from next
 
 export default function Project1() {
   return (
     <main className="bento-container">
       <div className="name-title-container">
-        <h1 className="name">Terug</h1>
+        <Link
+          href="/"
+          className="name"
+          style={{ textDecoration: "none", color: "inherit" }}
+        >
+          Terug
+        </Link>
         <span className="portfolio">Project 1</span>
       </div>
       <div className="horizontal-line"></div>
@@ -13,7 +20,7 @@ export default function Project1() {
       <div className="bento-grid">
         <div className="bento-box full-box">
           <Image
-            src=""
+            src="" // put your image path here
             alt="Full Project Image"
             width={1500}
             height={800}
